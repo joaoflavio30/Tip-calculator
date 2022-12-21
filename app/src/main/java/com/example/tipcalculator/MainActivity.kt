15d7ade69fe04461708c31f1f0708cdbaf4701a0
joaoflaviofreitas <joaoflaviofreitas@gmail.com>
2022-12-21 09:40:16 -0300

    simple project for calculate tips

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.tipcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+
+private var serviceRate = "Nothing"
+private val amazing = "amazing"
+private val good = "good"
+private const val ok = "ok"
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var input = findViewById<EditText>(R.id.editTextTextPersonName).text
+
+
+        val calculateBtn = findViewById<Button>(R.id.button2)
+        calculateBtn.setOnClickListener{
+            stateBtnRadio()
+            if(serviceRate!="Nothing" && input.isNotEmpty()) {
+                calc(input.toString())
+            }
+        }
+
+
+    }
+
+
+    private fun stateBtnRadio(){
+    val btnRadio2 = findViewById<RadioButton>(R.id.radioButton2)
+    val btnRadio3 = findViewById<RadioButton>(R.id.radioButton3)
+    val btnRadio4 = findViewById<RadioButton>(R.id.radioButton4)
+
+        if(btnRadio2.isChecked){
+            serviceRate = amazing
+        }else if(btnRadio3.isChecked){
+            serviceRate = good
+        }else if(btnRadio4.isChecked){
+            serviceRate = ok
+        }
+
+    }
+
+    private fun calc(number : String){
+        val resultCalc = findViewById<TextView>(R.id.textViewPrice)
+        when(serviceRate){
+            amazing -> resultCalc.text = (number.toDouble() * 1.2).toString()
+            good -> resultCalc.text = (number.toDouble() * 1.18).toString()
+            ok -> resultCalc.text = (number.toDouble() * 1.15).toString()
+        }
+    }
+
+
+
+}
+
+
